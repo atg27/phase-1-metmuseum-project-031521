673b90b868e2d13c95340172163f25a8ee0193f3
Andrew <atg27@uw.edu>
2021-04-06 14:56:45 -0700

    added HTML structure

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://collectionapi.metmuseum.org/public/collection/v1/departments")
-    .then
-    .then
-})
+const metIslamicArtUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=dog"   
+const metIslamicArtTopic ="https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q="   
+   
+   fetch(metIslamicArtUrl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
