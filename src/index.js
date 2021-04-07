0f2100196ca4dd8e8c588e6707ca265ed1b945ff
Andrew <atg27@uw.edu>
2021-04-07 13:01:52 -0700

    seperated fetch requests, completed append funx

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const metIArtTopicUrl ="https://collectionapi.metmuseum.org/public/collection/v1
 // truncated url for specific art piece object -Can add objectID at end
 const metIAObjectUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
 
-const artContainer = document.getElementById("art-content")
+//const artContainer = document.querySelector("div")
 
 
 
@@ -16,42 +16,64 @@ const artContainer = document.getElementById("art-content")
 function getObjectIdFromSearch(url) { 
    fetch(url)
     .then(res => res.json())
-    .then(function(data) {
+    .then((data) => {
         let objectIDArray = data.objectIDs;
         //console.log(objectIDArray)
         //console.log(objectIDArray[Math.floor(Math.random()* objectIDArray.length)+1])
         let randomlyGeneratedObjId = objectIDArray[Math.floor(Math.random() * objectIDArray.length)+1]
         
-       return fetch(metIAObjectUrl + randomlyGeneratedObjId)
-        .then(res => res.json())
-        //.then(console.log)
-        .then(function(data){
-            let artPieceImg = data.primaryImageSmall;
-            let artPieceTitle = data.title;
-            let artPieceDate = data.objectDate
-            //console.log(artPieceImg, artPieceTitle, artPieceDate) 
-            log(artPieceDate)
-            log(artPieceImg)
-            log(artPieceTitle)
+        getArt(randomlyGeneratedObjId)
 
+        // //2nd function and call it
+        // fetch(metIAObjectUrl + randomlyGeneratedObjId)
+        // .then(res => res.json())
+        // //.then(console.log)
+        // .then((data) => {
+        //     let artPieceImg = data.primaryImageSmall;
+        //     let artPieceTitle = data.title;
+        //     let artPieceDate = data.objectDate
+        //     //console.log(artPieceImg, artPieceTitle, artPieceDate) 
+        //     log(artPieceDate)
+        //     log(artPieceImg)
+        //     log(artPieceTitle)
+        //     const artContainer = document.querySelector("div")
+        //     renderArt(data)
             
         
-        })
+        // })
        //select random bunch from array
         //  
     })
  }
+function getArt(objectId){
+    fetch(metIAObjectUrl + objectId)
+    .then(res => res.json())
+    //.then(console.log)
+    .then((data) => {
+        let artPieceImg = data.primaryImageSmall;
+        let artPieceTitle = data.title;
+        let artPieceDate = data.objectDate
+        //console.log(artPieceImg, artPieceTitle, artPieceDate) 
+        log(artPieceDate)
+        log(artPieceImg)
+        log(artPieceTitle)
+        const artContainer = document.querySelector("div")
+        renderArt(data)
+        
+    
+    })
+}
 
 function renderArt(artPiece){
-
+        const artContainer = document.querySelector("div")
         let h2 = document.createElement('h2')
-        h2.innerText = artPiece.name
+        h2.innerText = artPiece.title
       
         let img = document.createElement('img')
         img.setAttribute('src', artPiece.primaryImageSmall)
       
         let p = document.createElement('p')
-        p.innerText = artPiece.title
+        p.innerText = artPiece.objectDate
       
         artContainer.append(h2,img, p)
     
