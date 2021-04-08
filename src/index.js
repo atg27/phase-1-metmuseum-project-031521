a6c956f85c1bb39e4a6c91d2fa1ea5511c7aa3ca
Andrew <atg27@uw.edu>
2021-04-07 20:59:05 -0700

    clean up js file, add more css

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
+const metIAObjectUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
 
 // url for "dog" keyword search in IA dept. Returns object: { total results = # , objectIDs = [array of individual object ids] } 
+const metIAIslam = "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=14&q=islam"
+const metIAClothingUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=clothing"
 const metIADoggoUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=dog"   
 const metIACatUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=cat"
 const metIAFoodUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=food"
 const metIABuildingUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=building"
-// truncated url for search by keyword in IA dept -Can add keywork to end
-const metIArtTopicUrl ="https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q="  
 
-// truncated url for specific art piece object -Can add objectID at end
-const metIAObjectUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
+//const metIArtTopicUrl ="https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q="  
 
-//const artContainer = document.querySelector("div")
-
-
-
-//funx to fetch random objectId from array + fetch selected object for display on SPA
 function getObjectIdFromSearch(url) { 
    fetch(url)
     .then(res => res.json())
@@ -25,28 +20,10 @@ function getObjectIdFromSearch(url) {
         let randomlyGeneratedObjId = objectIDArray[Math.floor(Math.random() * objectIDArray.length)+1]
         
         getArt(randomlyGeneratedObjId)
-
-        // //2nd function and call it
-        // fetch(metIAObjectUrl + randomlyGeneratedObjId)
-        // .then(res => res.json())
-        // //.then(console.log)
-        // .then((data) => {
-        //     let artPieceImg = data.primaryImageSmall;
-        //     let artPieceTitle = data.title;
-        //     let artPieceDate = data.objectDate
-        //     //console.log(artPieceImg, artPieceTitle, artPieceDate) 
-        //     log(artPieceDate)
-        //     log(artPieceImg)
-        //     log(artPieceTitle)
-        //     const artContainer = document.querySelector("div")
-        //     renderArt(data)
-            
-        
-        // })
-       //select random bunch from array
-        //  
     })
  }
+
+
 function getArt(objectId){
     fetch(metIAObjectUrl + objectId)
     .then(res => res.json())
@@ -64,7 +41,7 @@ function getArt(objectId){
     })
     .catch(error => {
         console.log('Error!', error)
-        
+
       })
 }
 
@@ -88,17 +65,5 @@ function log(input){
     console.log(input)
 }
 
-getObjectIdFromSearch(metIADoggoUrl);
+getObjectIdFromSearch(metIAIslam);
 
-
-
-
-//   .catch(function(error) {
-//     console.log('Error!', error)
-//   })
-
-
-
-// function randomValue(arr) {
-//     arr[Math.floor(Math.random()* objectIDArray.length)]
-// }
