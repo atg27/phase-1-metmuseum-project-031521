63a9fb9c9e1b0a11a72e244e9578eb99618b50c7
Andrew <atg27@uw.edu>
2021-04-08 12:30:54 -0700

    styling css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const metIAObjectUrl = "https://collectionapi.metmuseum.org/public/collection/v1/objects/"
 
-// url for "dog" keyword search in IA dept. Returns object: { total results = # , objectIDs = [array of individual object ids] } 
 const metIAIslam = "https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=14&q=islam"
 const metIAClothingUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=clothing"
 const metIADoggoUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=dog"   
@@ -8,21 +7,17 @@ const metIACatUrl = "https://collectionapi.metmuseum.org/public/collection/v1/se
 const metIAFoodUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=food"
 const metIABuildingUrl = "https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q=building"
 
-//const metIArtTopicUrl ="https://collectionapi.metmuseum.org/public/collection/v1/search?departmentId=14&q="  
-
 function getObjectIdFromSearch(url) { 
    fetch(url)
     .then(res => res.json())
     .then((data) => {
         let objectIDArray = data.objectIDs;
-        //console.log(objectIDArray)
-        //console.log(objectIDArray[Math.floor(Math.random()* objectIDArray.length)+1])
-        let randomlyGeneratedObjId = objectIDArray[Math.floor(Math.random() * objectIDArray.length)+1]
+        let randomlyGeneratedObjId = objectIDArray[Math.floor(Math.random() * objectIDArray.length)]
         
         getArt(randomlyGeneratedObjId)
     })
     .catch(error => {
-        console.log('Error!', error)
+        console.alert('Error!', error)
       })
  }
 
@@ -30,16 +25,13 @@ function getObjectIdFromSearch(url) {
 function getArt(objectId){
     fetch(metIAObjectUrl + objectId)
     .then(res => res.json())
-    //.then(console.log)
     .then((data) => {
         let artPieceImg = data.primaryImageSmall;
         let artPieceTitle = data.title;
         let artPieceDate = data.objectDate
-        //console.log(artPieceImg, artPieceTitle, artPieceDate) 
         log(artPieceDate)
         log(artPieceImg)
         log(artPieceTitle)
-        const artContainer = document.querySelector("div")
         renderArt(data)
     })
     .catch(error => {
@@ -67,5 +59,5 @@ function log(input){
     console.log(input)
 }
 
-getObjectIdFromSearch(metIAIslam);
+//getObjectIdFromSearch(metIAIslam);
 
